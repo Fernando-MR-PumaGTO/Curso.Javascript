30a56c5a6ed1b9b1213d99fe16dbc3e621ee071d
Fernando-MR-PumaGTO <94247309+Fernando-MR-PumaGTO@users.noreply.github.com>
2022-03-16 12:01:20 -0300

    Add files via upload

---
+++ Dog Therapy/main.js
@@ -0,0 +1,16 @@
+var image = document.querySelector('.dog-image');
+     
+function fetchDog() {
+  console.log("click registerd")
+  console.log(image);
+  fetch('https://dog.ceo/api/breeds/image/random').then(response => {
+  return response.json();  
+  
+  }).then(jsonResponse => {
+    image.src=jsonResponse.message;
+  }).catch(function(){
+    console.log('error on the coding')
+  })
+}
+
+fetchDog();
